0e51fef67878a05f2b59808e6ad4e19a7aa67860
paulodhiambo <paulodhiambo962@gmail.com>
2020-12-20 12:17:38 +0300

    minor fix

--- src/main/java/com/odhiambopaul/demo/controllers/TodoController.java
+++ src/main/java/com/odhiambopaul/demo/controllers/TodoController.java
@@ -18,17 +18,18 @@ public class TodoController {
         this.todoService = todoService;
     }
 
+    //The function receives a GET request, processes it and gives back a list of Todo as a response.
     @GetMapping
     public ResponseEntity<List<Todo>> getAllTodos() {
         List<Todo> todos = todoService.getTodos();
         return new ResponseEntity<>(todos, HttpStatus.OK);
     }
-
+    //The function receives a GET request with id in the url path, processes it and returns a Todo with the specified Id
     @GetMapping({"/{todoId}"})
     public ResponseEntity<Todo> getTodo(@PathVariable Long todoId) {
         return new ResponseEntity<>(todoService.getTodoById(todoId), HttpStatus.OK);
     }
-
+    //The function receives a POST request, processes it, creates a new Todo and saves it to the database and returns a resource link to the created todo.
     @PostMapping
     public ResponseEntity<Todo> saveTodo(@RequestBody Todo todo) {
         Todo todo1 = todoService.insert(todo);
@@ -36,13 +37,13 @@ public class TodoController {
         httpHeaders.add("todo", "/api/v1/todo/" + todo1.getId().toString());
         return new ResponseEntity<>(todo1, httpHeaders, HttpStatus.CREATED);
     }
-
+    //The function receives a PUT request, updates the Todo with the specified Id and returns the updated Todo
     @PutMapping({"/{todoId}"})
     public ResponseEntity<Todo> updateTodo(@PathVariable("todoId") Long todoId, @RequestBody Todo todo) {
         todoService.updateTodo(todoId, todo);
         return new ResponseEntity<>(todoService.getTodoById(todoId), HttpStatus.OK);
     }
-
+    //The function receives a DELETE request, deletes the Todo with the specified Id.
     @DeleteMapping({"/{todoId}"})
     public ResponseEntity<Todo> deleteTodo(@PathVariable("todoId") Long todoId) {
         todoService.deleteTodo(todoId);
